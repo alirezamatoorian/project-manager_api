20d24746cadb6e9d052ba6c1beae2ca7c8acac92
Alireza <ali.r.matourianpoor@gmail.com>
2025-10-02 14:18:14 +0330

    feat(admin.py):coding admin panel for users

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('phone', 'email', 'first_name', 'last_name', 'is_active', 'date_joined')
+    list_filter = ('is_active',)
+    search_fields = ('email', 'phone', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        # ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'email')}),
+        ('دسترسی‌ها', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('تاریخ‌ها', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active')}
+         ),
+    )
+    readonly_fields = ('date_joined',)
+
+
+admin.site.register(User, UserAdmin)
